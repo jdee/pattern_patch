4408fa57088946566e908f0bc41a84f23fa0b072
Jimmy Dee <jgvdthree@gmail.com>
2019-05-15 02:00:08 -0700

    Restore extension for 2.3. Added spex.

--- lib/pattern_patch/core_ext/hash.rb
+++ lib/pattern_patch/core_ext/hash.rb
@@ -1,8 +1,16 @@
 class Hash
+  # #transform_keys not available in 2.3
   def symbolize_keys
     each_with_object({}) do |ary, hash|
       key, value = *ary
       hash[key.to_sym] = value
     end
+  end
+
+  def symbolize_keys!
+    hash = symbolize_keys
+    clear
+    hash.each { |k, v| self[k] = v }
+    nil
   end
 end
